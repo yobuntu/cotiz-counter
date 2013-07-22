b8d6edc696a9d560037c46321ca4e06ec2609403
Yohann Rebattu <yohann@rebattu.fr>
2013-07-22 14:50:09 +0200

    new contributions

--- lpeManager/views/contribution.py
+++ lpeManager/views/contribution.py
@@ -2,10 +2,18 @@ from flask import request, session, g, redirect, url_for, abort, render_template
 from flaskext.babel import gettext as _, format_datetime
 
 from lpeManager import app
-from lpeManager.db import Member, Contribution
+from lpeManager.db import db, Member, Contribution
 from lpeManager.forms import ContributionForm
 
-@app.route('/contributions/add',methods=['GET','POST'])
+@app.route('/contributions/add/<member_id>',methods=['GET','POST'])
 def add_contribution(member_id):
-    pass
+    contribution = Contribution()
+    form = ContributionForm(obj=contribution)
+    if form.validate_on_submit():
+        form.populate_obj(contribution)
+        db.session.add(contribution)
+        db.session.commit()
+        flash(_("Contribution added for member"))
+        return redirect(url_for('member_list'))
+    return render_template('/contribution/add.html', form=form)
 
